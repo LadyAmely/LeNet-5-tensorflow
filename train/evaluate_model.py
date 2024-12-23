95ef1e8297e2b0fb3c9eded22f6c04922156af7d
Amelia <nawrotamelia3@gmail.com>
2024-12-23 23:15:05 +0100

    lenet-5-commit-6

--- train/evaluate_model.py
+++ train/evaluate_model.py
@@ -4,15 +4,17 @@ from dataset.dataset_loader import load_and_preprocess_data
 from utils.metrics import calculate_metrics
 def evaluate_model():
     _, _, x_test, y_test = load_and_preprocess_data()
-    model = tf.keras.models.load_model('../saved_models/lenet5.h5')
-    test_loss, test_accuracy = model.evaluate(x_test, y_test)
-    print(f"Test Loss: {test_loss:.4f}")
-    print(f"Test Accuracy: {test_accuracy:.4f}")
 
-    y_pred = np.argmax(model.predict(x_test), axis=1)
+    model_layer = tf.keras.layers.TFSMLayer('../saved_models/lenet5/', call_endpoint='serving_default')
+
+    predictions = model_layer(x_test)
+    if isinstance(predictions, dict):
+        predictions = predictions[list(predictions.keys())[0]]
+    predictions = predictions.numpy()
+
+    y_pred = np.argmax(predictions, axis=1)
     precision, recall, f1 = calculate_metrics(y_test, y_pred)
     print(f"Precision: {precision:.4f}, Recall: {recall:.4f}, F1-Score: {f1:.4f}")
-
 
 if __name__ == "__main__":
     evaluate_model()
